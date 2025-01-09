2e6a42827b61b843b25d4054f646f731ba9c3b27
Luis Moreno <luism@Luiss-Air.attlocal.net>
2025-01-08 23:51:21 -0500

    first exercisedone

--- sandbox/Sandbox/Program.cs
+++ sandbox/Sandbox/Program.cs
@@ -1,9 +1,16 @@
 using System;
+using System.Data;
 
 class Program
 {
     static void Main(string[] args)
     {
         Console.WriteLine("Hello World! This is the Sandbox Project.");
+        Console.WriteLine("hola papito");
+
+        Console.WriteLine("What is your favorite color?");
+        string color = Console.ReadLine();
+        Console.WriteLine(color);
     }
 }
+
